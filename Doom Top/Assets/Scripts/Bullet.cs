aa93300966e0bf6ff1a5d2c472004cad580e109b
naman-mittal <55953194+naman-mittal@users.noreply.github.com>
2021-11-23 23:07:49 +0530

    Minor changes

--- Doom Top/Assets/Scripts/Bullet.cs
+++ Doom Top/Assets/Scripts/Bullet.cs
@@ -2,9 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private float speed = 30.0f;
+    private Rigidbody rb;
     public string firedBy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+       // rb.AddForce(Vector3.forward * speed);
+    }
+
+   
+
     
 }
