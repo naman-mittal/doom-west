32fe3dde4da1615e4d7af9860adade570ffe24bf
naman-mittal <55953194+naman-mittal@users.noreply.github.com>
2021-12-08 10:23:30 +0530

    updated bullet.cs

--- Doom West/Assets/Scripts/Bullet.cs
+++ Doom West/Assets/Scripts/Bullet.cs
@@ -5,7 +5,27 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 30.0f;
-    public string firedBy;
+    public string firedBy // ENCAPSULATION
+    {
+        get
+        {
+            return _firedBy;
+        }
+        set
+        {
+            if(value.Equals("enemy") || value.Equals("player"))
+            {
+                _firedBy = value;
+            }
+            else
+            {
+                _firedBy = null;
+                Debug.Log("Invalid value");
+            }
+        }
+    }
+
+    private string _firedBy;
 
     // Update is called once per frame
     void Update()
